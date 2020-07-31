c62c848383a5e64b6bd4e17372745d7d9b795928
kyubumJang <jkb2221@khu.ac.kr>
2020-07-31 20:25:59 +0900

    ADD contact project

---
+++ contact/main.py
@@ -0,0 +1,101 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Contact:
+    def __init__(self, name, phone_number, e_mail, addr):
+        self.name = name
+        self.phone_number = phone_number
+        self.e_mail = e_mail
+        self.addr = addr
+
+    def print_info(self):
+        print("Name: ", self.name)
+        print("Phone Number: ", self.phone_number)
+        print("E-mail: ", self.e_mail)
+        print("Address: ", self.addr)
+
+def set_contact():
+    name = input("Name: ")
+    phone_number = input("Phone Number: ")
+    e_mail = input("E-mail: ")
+    addr = input("Address: ")
+    contact = Contact(name, phone_number, e_mail, addr)
+    return contact
+
+def delete_contact(contact_list, name):
+    for i, contact in enumerate(contact_list):
+        if contact.name == name:
+            del contact_list[i]
+
+def print_menu():
+    print("1. 연락처 입력")
+    print("2. 연락처 출력")
+    print("3. 연락처 삭제")
+    print("4. 종료")
+    menu = input("메뉴선택: ")
+    return int(menu)
+
+def print_contact(contact_list):
+    for contact in contact_list:
+        contact.print_info()
+
+def run():
+    Jang = Contact('장규범', '010-1234-5678', 'kyubumJ@python.com', 'Seoul')
+    Jang.print_info()
+    set_contact()
+
+def run2():
+    while 1:
+        menu = print_menu()
+        if menu == 4:
+            break
+
+def run3():
+    contact_list = []
+    while 1:
+        menu = print_menu()
+        if menu == 1:
+            contact = set_contact()
+            contact_list.append(contact)
+        elif menu == 4:
+            break
+
+def run4():
+    contact_list = []
+    while 1:
+        menu = print_menu()
+        if menu == 1:
+            contact = set_contact()
+            contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 4:
+            break
+
+def run5():
+    contact_list = []
+    while 1:
+        menu = print_menu()
+        if menu == 1:
+            contact = set_contact()
+            contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 3:
+            name = input("Name: ")
+            delete_contact(contact_list, name)
+        elif menu == 4:
+            break
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #run()
+    #run2()
+    #run3()
+    #run4()
+    run5()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
